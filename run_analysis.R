b83c01fc046033040df3563c74e7d632eecea4ee
Iuliia Nikitina <yuliya.nikitina.95@list.ru>
2020-07-28 17:16:28 +0300

    added reading data from 'test' and 'train' folders

--- run_analysis.R
+++ run_analysis.R
@@ -9,5 +9,12 @@ if(!file.exists("data")) {
   unzip("./data/dataset.zip", exdir = "data")
 }
 
+#read data from test
+x_test <- read.table("data/UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("data/UCI HAR Dataset/test/y_test.txt")
+subject_test <- read.table("data/UCI HAR Dataset/test/subject_test.txt")
 
-
+#read data from train
+x_train <- read.table("data/UCI HAR Dataset/test/X_train.txt")
+y_train <- read.table("data/UCI HAR Dataset/test/y_train.txt")
+subject_train <- read.table("data/UCI HAR Dataset/test/subject_train.txt")
