310893ce762a07b8209f57781ba73042400422b6
Iuliia Nikitina <yuliya.nikitina.95@list.ru>
2020-07-28 17:27:40 +0300

    merged train and test sets in one set

--- run_analysis.R
+++ run_analysis.R
@@ -15,6 +15,12 @@ y_test <- read.table("data/UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table("data/UCI HAR Dataset/test/subject_test.txt")
 
 #read data from train
-x_train <- read.table("data/UCI HAR Dataset/test/X_train.txt")
-y_train <- read.table("data/UCI HAR Dataset/test/y_train.txt")
-subject_train <- read.table("data/UCI HAR Dataset/test/subject_train.txt")
+x_train <- read.table("data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("data/UCI HAR Dataset/train/subject_train.txt")
+
+#Merges the training and the test sets to create one data set
+
+x <- rbind(x_train, x_test)
+y <- rbind(y_train, y_test)
+subject <- rbind(subject_train, subject_test)
